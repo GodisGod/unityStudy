770f9df1ac135629129d5959eb33c27c27726fd1
GodisGod <963140097@qq.com>
2022-11-22 03:25:15 +0800

    相机跟随1
    相机跟随2

--- Assets/Scrips/MoveController.cs
+++ Assets/Scrips/MoveController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,20 +9,53 @@ public class MoveController : MonoBehaviour
     Rigidbody2D rb2d;
 
     public float speed = 3.0f;
-    
+
+    Animator animator;
+    string animationState = "AnimationState";
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        
+    }
+
+    enum CharStates{
+        walkEast = 1,
+        walkSouth = 2,
+        walkWest = 3,
+        walkNorth = 4,
+        idleSouth = 5
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateState();
     }
 
     private void FixedUpdate()
+    {
+        MoveCharacter();
+        
+    }
+
+        private void UpdateState()
+    {
+        if (movement.x > 0) {
+            animator.SetInteger(animationState, (int)CharStates.walkEast);   
+        }else if (movement.x < 0) {
+            animator.SetInteger(animationState, (int)CharStates.walkWest);   
+        }else if (movement.y > 0) {
+            animator.SetInteger(animationState, (int)CharStates.walkNorth);   
+        }else if (movement.y < 0) {
+            animator.SetInteger(animationState, (int)CharStates.walkSouth);   
+        }else{
+            animator.SetInteger(animationState, (int)CharStates.idleSouth);   
+        }
+    }
+
+    private void MoveCharacter()
     {
         movement.x = Input.GetAxis("Horizontal");
         movement.y = Input.GetAxis("Vertical");
